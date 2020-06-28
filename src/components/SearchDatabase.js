ec3aaf5146a655929029850a9cf32dbb9df00acc
eminaydin <eminaydin93@yahoo.com>
2020-06-28 11:24:01 +0200

    Get the database

--- src/components/SearchDatabase.js
+++ src/components/SearchDatabase.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/SearchDatabase.css"
+import Moment from 'react-moment';
 
 const SearchDatabase = ({ calculationHistory }) => {
-    console.log(calculationHistory);
-
     return (
-        <div>
-            <div className="hey">
+        <div className="history-page">
+            <Link to="/">
+                <span className="back-btn">{"<"} Go back</span>
+            </Link>
+            <div className="conversion-history-table">
+                <table>
+                    <tr>
+                        <th>Date</th>
+                        <th>From</th>
+                        <th>To</th>
+                    </tr>
+                    {calculationHistory.map(({ amount, currencyTo, currencyFrom, multipliedAmount, date }) => {
+                        const formattedAmount = new Intl.NumberFormat('de-DE').format(amount)
+                        const formattedMultipliedAmount = new Intl.NumberFormat('de-DE').format(multipliedAmount);
+                        return < tr >
+                            <td><Moment format="LL">{date}</Moment></td>
+                            <td>{formattedAmount} {currencyFrom}</td>
+                            <td>{formattedMultipliedAmount} {currencyTo}</td>
+                        </tr>
+                    })}
+                </table>
             </div>
-            {calculationHistory.map(e => <p>{e.amount}</p>)}
-        </div>
+
+        </div >
     );
 }
 
